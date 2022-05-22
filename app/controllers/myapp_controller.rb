7f5532f04a910b556f5dd2c2fd49ee6cf57d9b48
Batuhan SONMEZER <batuhan.sonmezer@kellogg.northwestern.edu>
2022-05-22 06:03:48 +0000

    v2

--- app/controllers/myapp_controller.rb
+++ app/controllers/myapp_controller.rb
@@ -1,12 +1,19 @@
 class MyappController < ApplicationController
 
+
 def index
     @places = Place.all
 end
 
-def show
-    @place = Place.find_by({"place_id" => params ["place_id"] })
+def new
+    @place = Places.new
 end
+
+
+
+# def show
+#     @place = Place.find_by({"place_id" => params ["place_id"] })
+# end
 
 
 end
